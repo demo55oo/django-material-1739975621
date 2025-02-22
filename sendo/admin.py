870f071e6407af0daaf80016ccbe4a039da3c653
demo55oo <85605417+demo55oo@users.noreply.github.com>
2025-02-22 11:07:27 +0200

    Removed sensitive data

--- sendo/admin.py
+++ sendo/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customers, Order,IncomingMessage  # Import your models
+from .models import Customers, Order,IncomingMessage ,Message, ChatSession # Import your models
 
 admin.site.register(Customers)
 admin.site.register(Order)
+admin.site.register(IncomingMessage)
+admin.site.register(Message)
+admin.site.register(ChatSession)
